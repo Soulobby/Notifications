83b9062e2e072e9e4c4ca8148cb8e4c25f49bfe7
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-12-01 08:28:46 +0000

    feat: Add Santa notifications (#1)

--- source/constants.ts
+++ source/constants.ts
@@ -2,4 +2,7 @@ export const Role = {
 	ApmekenAmethyst: "583288933773869066",
 	ScabariteCrystal: "583289583911960589",
 	MenaphiteGifts: "615200253087449091",
+	Santa: "1180053189807067186",
 } as const satisfies Readonly<Record<string, `${bigint}`>>;
+
+export const CHRISTMAS_EVENT_END_TIMESTAMP = Date.UTC(2_024, 0, 8);
--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,5 @@
 import { Jewel, jewel, stock } from "runescape";
-import { Role } from "./constants.js";
+import { CHRISTMAS_EVENT_END_TIMESTAMP, Role } from "./constants.js";
 import { roleMention } from "./utility.js";
 
 interface Env {
@@ -7,20 +7,30 @@ interface Env {
 }
 
 export default {
-	async scheduled(_, { WEBHOOK_URL }) {
-		const currentJewel = jewel();
+	async scheduled({ scheduledTime }, { WEBHOOK_URL }) {
+		const date = new Date(scheduledTime);
 		const contents = [];
 
-		if (currentJewel) {
-			contents.push(
-				`The ${roleMention(
-					currentJewel === Jewel.ApmekenAmethyst ? Role.ApmekenAmethyst : Role.ScabariteCrystal,
-				)} is accessible today.`,
-			);
+		if (date.getUTCHours() === 0) {
+			const currentJewel = jewel();
+
+			if (currentJewel) {
+				contents.push(
+					`The ${roleMention(
+						currentJewel === Jewel.ApmekenAmethyst ? Role.ApmekenAmethyst : Role.ScabariteCrystal,
+					)} is accessible today.`,
+				);
+			}
+
+			if (stock().some((slot) => slot.includes("Menaphite"))) {
+				contents.push(`The Travelling Merchant has ${roleMention(Role.MenaphiteGifts)} in stock today!`);
+			}
 		}
 
-		if (stock().some((slot) => slot.includes("Menaphite"))) {
-			contents.push(`The Travelling Merchant has ${roleMention(Role.MenaphiteGifts)} in stock today!`);
+		if (date.getTime() < CHRISTMAS_EVENT_END_TIMESTAMP && date.getUTCHours() === 20) {
+			// Santa leaves 2 hours after their arrival.
+			const leave = Math.floor((date.getTime() + 7_200_000) / 1_000);
+			contents.push(`${roleMention(Role.Santa)} has arrived and will leave <t:${leave}:R>!`);
 		}
 
 		for (const content of contents) {
